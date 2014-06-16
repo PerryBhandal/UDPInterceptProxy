c3c85136527d138700e7464a8f4b1a3e328a0f7f
Perry Bhandal <perry.bhandal@gmail.com>
2014-06-16 15:23:13 -0400

    Listen now returns length rather than the buffer. Buffer should be accessed through the member instance mBuffer, with the number of bytes to read returned from listen.

--- UDPSocket.c
+++ UDPSocket.c
@@ -6,13 +6,13 @@
 #include "UDPSocket.h"
 
 UDPSocket::UDPSocket(char* ip, unsigned short port) {
+	mCliAddr = mBindAddr = NULL;
 	mBuffer = new char[1000];
 	mSocketFD = new int;
 	bindSocket(ip, port);
 }
 
 void UDPSocket::bindSocket(char* ip, unsigned short port) {
-	struct sockaddr_in bindAddr;
 
 	//Create our socket
 	*mSocketFD = socket(AF_INET, SOCK_DGRAM, 0);
@@ -22,34 +22,28 @@ void UDPSocket::bindSocket(char* ip, unsigned short port) {
     	//TODO: Add exit here.
    	}
 
-   	//Assemble our binding data
-   	bindAddr.sin_family = AF_INET;
-   	
-   	bindAddr.sin_port = htons(port);
-
+   	mBindAddr = getSockStruct(ip, port);
    	//Bind our socket
-   	if (bind(*mSocketFD, (struct sockaddr *) &bindAddr, sizeof(bindAddr)) < 0) {
+   	if (bind(*mSocketFD, (struct sockaddr *) mBindAddr, sizeof(mBindAddr)) < 0) {
    		fprintf(stderr, "Error while binding.");
    		//TODO: Add exit
    	}
 }
 
-char* UDPSocket::listen() {
-	printf("here");
-	struct sockaddr cliAddr;
+int UDPSocket::listen() {
 	int n;
 	socklen_t len;
 
-	len = sizeof(cliAddr);
-	n = recvfrom(*mSocketFD, mBuffer, 1000, 0, (struct sockaddr*) &cliAddr, &len);
-	return mBuffer;	
+	len = sizeof(mCliAddr);
+	n = recvfrom(*mSocketFD, mBuffer, 1000, 0, (struct sockaddr*) mCliAddr, &len);
+	return n;
 }
 
 void UDPSocket::send(char* toSend, int length) {
 	//sendto(*mSocket, toSend, length, 0, (struct sockaddr *) &cliAddr, sizeof(cliAddr));
 }
 
-struct sockaddr_in* getSockStruct(char* ip, unsigned short port) {
+struct sockaddr_in* UDPSocket::getSockStruct(char* ip, unsigned short port) {
 	struct sockaddr_in* sockStruct = new struct sockaddr_in;
 	bzero(sockStruct, sizeof(*sockStruct));
 	sockStruct->sin_family = AF_INET;
@@ -58,6 +52,12 @@ struct sockaddr_in* getSockStruct(char* ip, unsigned short port) {
 }
 
 UDPSocket::~UDPSocket() {
+	if (mCliAddr != NULL)
+		delete mCliAddr;
+
+	if (mBindAddr != NULL)
+		delete mBindAddr;
+
 	delete mSocketFD;
 	delete [] mBuffer;
 }
--- UDPSocket.h
+++ UDPSocket.h
@@ -8,6 +8,8 @@ class UDPSocket {
 	protected:
 		char* mBuffer;
 		int* mSocketFD;
+		struct sockaddr_in* mCliAddr;
+		struct sockaddr_in* mBindAddr;
 		struct sockaddr_in* getSockStruct(char* ip, unsigned short port);
 		virtual void bindSocket(char* ip, unsigned short port);
 };
