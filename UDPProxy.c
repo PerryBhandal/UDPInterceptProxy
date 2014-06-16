70c7d20e55ef79ac49b43f8513af7921e1e8cafd
Perry Bhandal <perry.bhandal@gmail.com>
2014-06-16 15:07:41 -0400

    Initial import

---
+++ UDPProxy.c
@@ -0,0 +1,18 @@
+#include "UDPProxy.h"
+#include "UDPSocket.h"
+#include <stdio.h>
+
+UDPProxy::UDPProxy(char* srcIP, unsigned short port) {
+	initialize(srcIP, port);
+}
+
+void UDPProxy::initialize(char* srcIP, unsigned short port) {
+	mSocket = new UDPSocket(srcIP, port);
+}
+
+
+int main(int argc, char* argv[]) {
+	printf("start\n");
+	char ip[] = "127.0.0.1";
+	UDPProxy test(ip, 5200);
+}
---
+++ UDPProxy.h
@@ -0,0 +1,11 @@
+class UDPSocket;
+
+class UDPProxy {
+	public:
+		UDPProxy(char* srcIP, unsigned short port);
+		void startProxy();
+
+	private:
+		UDPSocket* mSocket;
+		void initialize(char* srcIP, unsigned short port);
+};
